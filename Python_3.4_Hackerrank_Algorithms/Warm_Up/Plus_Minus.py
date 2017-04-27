c347273c71a08eb446be5d24418ee702ac97734b
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 15:34:51 -0500

    Update Plus_Minus.py

--- Python_3.4_Hackerrank_Algorithms/Warm_Up/Plus_Minus.py
+++ Python_3.4_Hackerrank_Algorithms/Warm_Up/Plus_Minus.py
@@ -1,1 +1,23 @@
 
+import sys
+
+n = int(input().strip())
+# n = total number of items in the array
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+
+pos = 0
+neg = 0
+zeros = 0
+
+for i in range(n):
+    if arr[i] == 0:
+        zeros += 1
+    elif arr[i] < 0:
+        neg += 1
+    else:
+        pos += 1
+
+        
+print(float(pos/n))
+print(float(neg/n))
+print(float(zeros/n))
