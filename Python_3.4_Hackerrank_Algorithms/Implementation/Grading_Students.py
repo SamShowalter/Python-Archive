6ee6affadceaa2ad7b54013471cae190f1b81608
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 18:37:17 -0500

    Update Grading_Students.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Grading_Students.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Grading_Students.py
@@ -1,1 +1,16 @@
 
+import sys
+
+#Partition the grades based on the parameters of the problem.
+n = int(input().strip())
+for a0 in range(n):
+    grade = int(input().strip())
+    if (grade < 38):
+        print(grade)
+    elif ((grade - 1) % 5 == 0 or (grade - 2) % 5 == 0) or (grade%5 == 0):
+        print(grade)
+    else:
+        if(grade + 1) % 5 == 0 and (grade >= 38):
+            print(grade + 1)
+        else:
+            print(grade + 2)
