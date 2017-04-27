002170428a68161517c3560f5ed593ed98ad6f75
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 15:07:04 -0500

    Update Very_Big_Sum.py

--- Python_3.4_Hackerrank_Algorithms/Warm_Up/Very_Big_Sum.py
+++ Python_3.4_Hackerrank_Algorithms/Warm_Up/Very_Big_Sum.py
@@ -1,1 +1,12 @@
 
+import sys
+
+#Python is the best for very big numbers
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+
+sum = 0
+for i in range(n):
+    sum += arr[i]
+    
+print(sum)
