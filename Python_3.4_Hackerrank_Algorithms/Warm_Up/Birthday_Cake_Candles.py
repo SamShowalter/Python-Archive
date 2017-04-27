f4d96420bdc84c5c6e32b868aeda342c91c78576
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 15:54:06 -0500

    Update Birthday_Cake_Candles.py

--- Python_3.4_Hackerrank_Algorithms/Warm_Up/Birthday_Cake_Candles.py
+++ Python_3.4_Hackerrank_Algorithms/Warm_Up/Birthday_Cake_Candles.py
@@ -1,1 +1,9 @@
 
+#Find the number of the tallest candles in the birthday cake with the built in count function
+import sys
+from collections import Counter
+
+n = int(input().strip())
+height = [int(height_temp) for height_temp in input().strip().split(' ')]
+
+print(height.count(max(height)))
