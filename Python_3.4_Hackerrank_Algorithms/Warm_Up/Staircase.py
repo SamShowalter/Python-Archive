ce6878332f2fb3aaa28cdd251881775da8643edd
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 15:36:25 -0500

    Update Staircase.py

--- Python_3.4_Hackerrank_Algorithms/Warm_Up/Staircase.py
+++ Python_3.4_Hackerrank_Algorithms/Warm_Up/Staircase.py
@@ -1,1 +1,10 @@
+import sys
 
+n = int(input().strip())
+
+def stairs(n):
+    for i in range(1, n + 1):
+        print(" "*(n - i) + "#"*i)
+        
+#This can also be done easily with recursion
+stairs(n)
