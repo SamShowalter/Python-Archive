ea5d77b0b7a40952ebbf6ddd277bea7a1d6071cb
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 18:51:09 -0500

    Update Migratory_Birds.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Migratory_Birds.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Migratory_Birds.py
@@ -1,1 +1,21 @@
 
+import sys
+from collections import Counter
+
+n = int(input().strip())
+types = list(map(int, input().strip().split(' ')))
+# your code goes here
+
+#Applies counter properties to the list types
+#See more about Counter at https://docs.python.org/3/library/collections.html
+newtype = Counter(types)
+
+mcount = max((newtype.values()))
+newtype = dict(newtype)
+
+finalist = []
+for i in newtype:
+    if newtype[i] == mcount:
+        finalist.append(i)
+        
+print(min(finalist))
