81467f453371211cb95266909a5a704c05a359b4
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 18:52:16 -0500

    Update Bon_Appetit.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Bon_Appetit.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Bon_Appetit.py
@@ -1,1 +1,15 @@
 
+#Lots and lots of inputs
+list = [int(num) for num in input().strip().split(' ')]
+food = [int(food) for food in input().strip().split(' ')]
+bill = int(input().strip())
+
+n = list[0]
+gross = list[1]
+                            
+shePays = (sum(food) - food[gross])//2
+
+if shePays == bill:
+    print("Bon Appetit")
+else:
+    print(bill - shePays)
