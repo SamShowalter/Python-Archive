84d071af971499f6cb7fb7c9c7772c5ea1f0759d
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 15:37:50 -0500

    Update Mini_Max_Sum.py

--- Python_3.4_Hackerrank_Algorithms/Warm_Up/Mini_Max_Sum.py
+++ Python_3.4_Hackerrank_Algorithms/Warm_Up/Mini_Max_Sum.py
@@ -1,1 +1,15 @@
 
+import sys
+
+#Just remove the largest or smallest item from the set
+a,b,c,d,e = input().strip().split(' ')
+listmin = [int(a),int(b),int(c),int(d),int(e)]
+listmax = [int(a),int(b),int(c),int(d),int(e)]
+
+def minlist(list):
+    return sum(list) - max(list)
+
+def maxlist(list):
+    return sum(list) - min(list)
+
+print(str(minlist(listmin)) + " " + str(maxlist(listmax)))
