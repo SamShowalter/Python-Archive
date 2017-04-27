35eb639742d14e957b1c04118b0f148143ce8881
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 18:55:23 -0500

    Update Drawing_Book.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Drawing_Book.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Drawing_Book.py
@@ -1,1 +1,18 @@
 
+import sys
+
+#n is total pages, p is the page the teacher wants her to turn to.
+n = int(input().strip())
+p = int(input().strip())
+# your code goes here
+
+if n % 2 == 0:
+    if (n - p) <= p:
+        print((n-p)//2)
+    else:
+        print(p//2)
+else:
+    if (n - p) <= p:
+        print((n-p)//2)
+    else:
+        print(p//2)
