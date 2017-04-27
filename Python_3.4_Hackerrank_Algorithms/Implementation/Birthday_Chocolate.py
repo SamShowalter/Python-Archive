d939faf6c55df76d916e3d4acef9f148ca9c1605
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 18:44:02 -0500

    Update Birthday_Chocolate.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Birthday_Chocolate.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Birthday_Chocolate.py
@@ -1,1 +1,22 @@
 
+import sys
+
+#Find the number of ways to break up the chocolate
+def getWays(squares, d, m):
+    count = 0
+    if len(squares) < m:
+        return 0
+    if len(squares) == 1 and squares[0] == d and m == 1:
+        return 1
+    for i in range(n - m + 1):
+        if sum(choc[i:i+m]) == d: 
+            count += 1
+    return count
+                
+
+n = int(input().strip())
+choc = list(map(int, input().strip().split(' ')))
+d,m = input().strip().split(' ')
+d,m = [int(d),int(m)]
+result = getWays(choc, d, m)
+print(result)
