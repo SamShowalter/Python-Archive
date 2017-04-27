aa0af8b7318300a6f33a6167abc2d3da1ed3dac2
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 18:53:39 -0500

    Update Sock_Merchant.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Sock_Merchant.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Sock_Merchant.py
@@ -1,1 +1,19 @@
 
+import sys
+from collections import Counter
+
+#Inputs
+n = int(input().strip())
+c = [int(c_temp) for c_temp in input().strip().split(' ')]
+
+#Setting up necessary variables and properties
+temp = Counter(c)
+final = 0
+
+for i in temp:
+    if temp[i] % 2 == 0:
+        final += (temp[i]//2)
+    else:
+        final += (temp[i] - 1)//2
+
+print(final)
