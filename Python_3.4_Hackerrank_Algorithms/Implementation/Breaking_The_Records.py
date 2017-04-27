45275af75ba8787235ec8b854adaa1ebe3c52177
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 18:42:30 -0500

    Update Breaking_The_Records.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Breaking_The_Records.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Breaking_The_Records.py
@@ -1,1 +1,21 @@
 
+import sys
+
+#Another easy one. If you use custom input you can get this one on the first try.
+n = int(input().strip())
+score = list(map(int, input().strip().split(' ')))
+# your code goes here
+
+mini = score[0]
+maxi = score[0]
+mincount = 0
+maxcount = 0
+for i in range (1,n):
+    if score[i] > maxi:
+        maxcount += 1
+        maxi = score[i]
+    elif score[i] < mini:
+        mincount += 1
+        mini = score[i]
+ 
+print(str(maxcount) + ' ' + str(mincount))
