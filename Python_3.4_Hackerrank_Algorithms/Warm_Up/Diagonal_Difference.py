001337c1c3c43c1ce572a116ba33a90180bc8770
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 15:21:07 -0500

    Update Diagonal_Difference.py

--- Python_3.4_Hackerrank_Algorithms/Warm_Up/Diagonal_Difference.py
+++ Python_3.4_Hackerrank_Algorithms/Warm_Up/Diagonal_Difference.py
@@ -1,1 +1,31 @@
 
+import sys
+
+n = int(input().strip())
+a = []
+
+#Generate a list of the rows (list of lists)
+for a_i in range(n):
+    a_t = [int(a_temp) for a_temp in input().strip().split(' ')]
+    a.append(a_t)
+    
+#Primary Diagonal sum
+def pdsum(a):
+    pdsum = 0
+    n = len(a)
+    for i in range(n):
+        #take the first item from the first list, the second item from the second...
+        pdsum += a[i][i]
+    return pdsum
+
+#Secondary diagonal sum
+def sdsum(a):
+    sdsum = 0
+    n = len(a)
+    nindex = n - 1 
+    for i in range(n):
+        #take the last item from the first list, the second-last item from the second...
+        sdsum += a[i][nindex - i]
+    return sdsum
+
+print(abs(pdsum(a) - sdsum(a)))
