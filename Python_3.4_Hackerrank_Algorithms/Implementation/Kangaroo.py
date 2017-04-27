3d6625a3c4b28c1ab431ae03e6a2b03be1c559b5
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 18:39:55 -0500

    Update Kangaroo.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Kangaroo.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Kangaroo.py
@@ -1,1 +1,21 @@
 
+import sys
+
+#Another easy one. Just a lot of parameters to keep track of.
+x1,v1,x2,v2 = input().strip().split(' ')
+x1,v1,x2,v2 = [int(x1),int(v1),int(x2),int(v2)]
+
+k1 = x1
+k2 = x2
+def kangaroo(k1,k2,v1,v2):
+    count = 0
+    while count < 10000:
+        if k1 == k2:
+            print("YES")
+            return
+        k1 = k1 + v1
+        k2 = k2 + v2
+        count += 1
+    print("NO")
+    
+kangaroo(k1,k2,v1,v2)
