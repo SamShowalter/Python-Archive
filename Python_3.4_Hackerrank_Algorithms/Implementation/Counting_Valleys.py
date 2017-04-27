5b4cba197053d29d931fe3699aecd3f57ac03392
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 18:56:24 -0500

    Update Counting_Valleys.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Counting_Valleys.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Counting_Valleys.py
@@ -1,1 +1,18 @@
 
+n = int(input().strip())
+s= input().strip()
+
+level = 0
+valley = 0
+
+for i in range(len(s)):
+    if s[i] == "U":
+        level += 1
+        status = "up"
+    else:
+        level -= 1
+        status = "down"
+    if level == 0 and status == "up":
+        valley += 1
+
+print(valley)
