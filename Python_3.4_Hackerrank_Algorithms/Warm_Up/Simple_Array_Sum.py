20633c054064b185526ebbfff85df06864f2ef68
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 13:19:23 -0500

    Update Simple_Array_Sum.py

--- Python_3.4_Hackerrank_Algorithms/Warm_Up/Simple_Array_Sum.py
+++ Python_3.4_Hackerrank_Algorithms/Warm_Up/Simple_Array_Sum.py
@@ -1,1 +1,14 @@
 
+import sys
+
+n = int(input().strip())
+arr = [int(s) for s in input().strip().split(' ')]
+
+#Sums up all values in array
+def arraySum(arr):
+    sum = 0
+    for i in arr:
+        sum += i
+    return sum
+        
+print(arraySum(arr))
