0608a5aa4654ded4718d1b3e8da35434cd65aa82
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 18:46:30 -0500

    Update Divisible_Sum_Pairs.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Divisible_Sum_Pairs.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Divisible_Sum_Pairs.py
@@ -1,1 +1,16 @@
 
+import sys
+
+#Use floor division to get the rounded-down whole number
+n,k = input().strip().split(' ')
+n,k = int(n),int(k)
+list = [int(a_temp) for a_temp in input().strip().split(' ')]
+
+count = 0
+for i in range(n):
+    for j in range(n):
+        #Full disclosure, this has terrible runtime.
+        if i != j and ((list[i] + list[j]) % k == 0):
+            count += 1
+            
+print(count//2)
