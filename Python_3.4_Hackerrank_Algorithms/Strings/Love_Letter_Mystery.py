f83c37eae23b5c35ed966e8108f3a33d1cf8d6a4
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 12:28:57 -0500

    Update Love_Letter_Mystery.py

--- Python_3.4_Hackerrank_Algorithms/Strings/Love_Letter_Mystery.py
+++ Python_3.4_Hackerrank_Algorithms/Strings/Love_Letter_Mystery.py
@@ -1,1 +1,21 @@
 
+n = int(input().strip())
+
+for a0 in range(n):
+    count = 0
+    s = input().strip()
+    leng = len(s)
+    
+    if leng % 2 == 0:
+        left = s[:leng//2]
+        right = s[leng//2:]
+    else:
+        left = s[:leng//2]
+        right = s[leng//2 + 1:]
+    
+    #Reverse the order
+    left = left[::-1]
+    
+    for i in range(len(left)):
+        count += abs(ord(left[i]) - ord(right[i]))
+    print(count)
