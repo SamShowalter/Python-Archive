bd8d9dfe44410cc898d0d74550e3ee0340abd713
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 19:06:04 -0500

    Update The_Hurdle_Race.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/The_Hurdle_Race.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/The_Hurdle_Race.py
@@ -1,1 +1,14 @@
 
+import sys
+
+#fairly straightforward
+n,k = input().strip().split(' ')
+n,k = [int(n),int(k)]
+height = list(map(int, input().strip().split(' ')))
+bevs = 0
+maxh = max(height)
+
+if maxh > k:
+    bevs = (maxh - k)
+
+print(int(bevs))
