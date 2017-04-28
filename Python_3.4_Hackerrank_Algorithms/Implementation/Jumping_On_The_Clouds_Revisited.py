fad2f93d689d937dc9c68143fa149c96ec1257ed
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 20:05:20 -0500

    Update Jumping_On_The_Clouds_Revisited.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Jumping_On_The_Clouds_Revisited.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Jumping_On_The_Clouds_Revisited.py
@@ -1,1 +1,18 @@
 
+import sys
+
+
+n,k = input().strip().split(' ')
+n,k = [int(n),int(k)]
+c = [int(c_temp) for c_temp in input().strip().split(' ')]
+cloud = 0
+E = 100
+
+while cloud % n != 0 or E == 100:
+    cloud += k
+    E -= 1
+    if c[(cloud % n)] == 1:
+        E -= 2
+        
+print(E)
+
