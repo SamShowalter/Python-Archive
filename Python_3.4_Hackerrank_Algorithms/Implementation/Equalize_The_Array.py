876a9656b118e38e2a0018d9c97457a8aefe2249
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 20:14:24 -0500

    Update Equalize_The_Array.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Equalize_The_Array.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Equalize_The_Array.py
@@ -1,1 +1,15 @@
 
+
+count = int(input().strip())
+klist = [int(num) for num in input().strip().split(' ')]
+most = 0
+num = 0
+length = len(klist)
+#Look up what the key=.count function does online.
+most = max(set(klist), key=klist.count)
+
+for i in range(length):
+    if klist[i] == most:
+        num += 1
+        
+print(length - num)
