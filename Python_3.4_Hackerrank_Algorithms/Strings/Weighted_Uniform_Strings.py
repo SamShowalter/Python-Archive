18742e4ce2f9c5dee8f39a85aa5dfb74f924eeb9
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 12:18:02 -0500

    Update Weighted_Uniform_Strings.py

--- Python_3.4_Hackerrank_Algorithms/Strings/Weighted_Uniform_Strings.py
+++ Python_3.4_Hackerrank_Algorithms/Strings/Weighted_Uniform_Strings.py
@@ -1,1 +1,31 @@
 
+#!/bin/python3
+
+import sys
+
+s = input().strip()
+n = int(input().strip())
+all_val = get_set(s)
+
+def get_set(s):
+    # Get the first count, used for adding weights
+    weight = ord(s[0]) - 96
+    
+    # The set of each value
+    weights = set()
+    weights.add(weight)
+    
+    for i in range(1, len(s)):
+        # Duplicate, add the count
+        if s[i] == s[i - 1]:
+            weight += ord(s[i]) - 96
+        else:
+            weight = ord(s[i]) - 96
+        weights.add(weight)
+    return weights
+
+
+for a0 in range(n):
+    x = int(input().strip())
+    # your code goes here
+    print('Yes' if x in all_val else 'No')
