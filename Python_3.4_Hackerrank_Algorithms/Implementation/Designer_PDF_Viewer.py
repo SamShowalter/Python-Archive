05e77a7c1a6633e737cb906bcfcd46cefc431da9
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 19:07:15 -0500

    Update Designer_PDF_Viewer.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Designer_PDF_Viewer.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Designer_PDF_Viewer.py
@@ -1,1 +1,16 @@
 
+import sys
+
+h = list(map(int, input().strip().split(' ')))
+abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+dict = {}
+word = input().strip()
+maxH = 0
+
+for i in word:
+    temp = abc.index(i)
+    tempH = h[temp]
+    if tempH > maxH:
+        maxH = tempH
+
+print(len(word)*maxH)
