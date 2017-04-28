01fba92d5afc618e20ff0d82d7149e878ece9825
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 19:58:07 -0500

    Update Angry_Professor.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Angry_Professor.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Angry_Professor.py
@@ -1,1 +1,20 @@
 
+import sys
+
+t = int(input().strip())
+
+for a0 in range(t):
+    n,k = input().strip().split(' ')
+    n,k = [int(n),int(k)]
+    a = [int(a_temp) for a_temp in input().strip().split(' ')]
+    late = 0
+    for i in a:
+        if i > 0:
+            late += 1
+    if (n-late) < k:
+        print("YES")
+    else:
+        print("NO")
+
+
+            
