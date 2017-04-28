9ce9b825695dc1f5972fd081a62b1f18ed88c456
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 20:01:59 -0500

    Update Save_The_Prisoner.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Save_The_Prisoner.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Save_The_Prisoner.py
@@ -1,1 +1,10 @@
 
+count = int(input().strip())
+
+for t in range(count):
+    [n, m, s] = map(int, input().split(' '))
+    res = (s - 1 + m) % n
+    if res != 0:
+        print(res)
+    else:
+        print(n)
