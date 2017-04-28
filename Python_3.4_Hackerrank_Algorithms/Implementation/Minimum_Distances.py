99bdca98e520dcd773c8ff859a1059adc7a83b9c
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 20:20:14 -0500

    Update Minimum_Distances.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Minimum_Distances.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Minimum_Distances.py
@@ -1,1 +1,22 @@
 
+#!/bin/python3
+
+import sys
+from collections import Counter
+
+n = int(input().strip())
+A = [int(A_temp) for A_temp in input().strip().split(' ')]
+
+mincount = n
+for i in range(n):
+    count = 0
+    if A.count(A[i]) > 1:
+        for j in range(i+ 1, n):
+            if A[i] == A[j]:
+                count = j - i
+                if count < mincount:
+                    mincount = count
+if mincount == n:
+    print(-1)
+else:
+    print(mincount)
