0ab3f519abee9a4f474ea9c34cdc300cd8cacda8
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 20:00:03 -0500

    Update Viral_Advertising.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Viral_Advertising.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Viral_Advertising.py
@@ -1,1 +1,19 @@
 
+inpt = int(input().strip())
+
+def viral(days):
+    day = 1
+    count = 5
+    vsum = 2
+    if days == 1:
+        return vsum
+    else:
+        while day < days:
+            count = ((count//2)*3)
+            vsum += (count//2)
+            day += 1
+    return vsum
+
+print(viral(inpt))
+        
+        
