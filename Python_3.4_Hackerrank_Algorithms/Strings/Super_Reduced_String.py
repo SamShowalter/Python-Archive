6b6aba017de4de437a9abf5f20426ec0b81f8a8b
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 11:05:39 -0500

    Update Super_Reduced_String.py

--- Python_3.4_Hackerrank_Algorithms/Strings/Super_Reduced_String.py
+++ Python_3.4_Hackerrank_Algorithms/Strings/Super_Reduced_String.py
@@ -1,1 +1,14 @@
 
+s = input().strip()
+i = 0
+
+while i < len(s)-1:
+    if s[i]==s[i+1]:
+        s = s[:i] + s[i+2:]
+        i = 0
+    else:
+        i += 1
+
+print(s if s else 'Empty String')
+    
+    
