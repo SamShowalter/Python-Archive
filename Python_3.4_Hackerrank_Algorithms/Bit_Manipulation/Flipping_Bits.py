a07b7c1d661eaa8065705196373dcae46610f4a2
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 14:50:12 -0500

    Update Flipping_Bits.py

--- Python_3.4_Hackerrank_Algorithms/Bit_Manipulation/Flipping_Bits.py
+++ Python_3.4_Hackerrank_Algorithms/Bit_Manipulation/Flipping_Bits.py
@@ -1,1 +1,11 @@
 
+n = int(input().strip())
+
+for a0 in range(n):
+    t = input().strip()
+    temp = bin(int(t))
+    temp = temp[2:]
+    x = '11111111111111111111111111111111'
+    
+    final = str(int(x) - int(temp))
+    print(str(int(final,2)))
