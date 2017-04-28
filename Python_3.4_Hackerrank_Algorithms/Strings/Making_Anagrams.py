89e6ed82f6bd87c921adaa8f260e787bd60aac62
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 13:04:57 -0500

    Update Making_Anagrams.py

--- Python_3.4_Hackerrank_Algorithms/Strings/Making_Anagrams.py
+++ Python_3.4_Hackerrank_Algorithms/Strings/Making_Anagrams.py
@@ -1,1 +1,19 @@
 
+from collections import Counter
+
+a = [str(num) for num in input().strip()]
+b = [str(num) for num in input().strip()]
+
+la = len(a)
+lb = len(b)
+count = 0
+
+for i in range(la):
+    if a[i] in b:
+        count += 1
+        b.remove(a[i])
+    
+total = (la + lb - 2*count)
+
+print(total)
+       
