df1346b1526d4e1388e575f795079b1bb6addfba
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 13:11:00 -0500

    Update Two_Strings.py

--- Python_3.4_Hackerrank_Algorithms/Strings/Two_Strings.py
+++ Python_3.4_Hackerrank_Algorithms/Strings/Two_Strings.py
@@ -1,1 +1,16 @@
 
+n = int(input().strip())
+
+for a0 in range(n):
+    count = 0
+    a = str(input().strip())
+    b = str(input().strip())
+    
+    for i in a:
+        if i in b:
+            count += 1
+    if count > 1:
+        print("YES")
+    else:
+        print("NO")
+    
