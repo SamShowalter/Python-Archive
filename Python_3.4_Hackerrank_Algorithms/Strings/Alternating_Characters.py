904bdcb79456cc9861363ef626f26267ccac383f
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 12:26:59 -0500

    Update Alternating_Characters.py

--- Python_3.4_Hackerrank_Algorithms/Strings/Alternating_Characters.py
+++ Python_3.4_Hackerrank_Algorithms/Strings/Alternating_Characters.py
@@ -1,1 +1,13 @@
 
+n = int(input().strip())
+
+for a0 in range(n):
+    temp = input().strip()
+    count = 0
+    
+    for i in range(len(temp) - 1):
+        if temp[i] == temp[i+ 1]:
+            count += 1
+    print(count)
+    
+    
