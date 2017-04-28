b3feb2cde3f22ce81ce8c23de34df49382a878a0
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 15:56:05 -0500

    Update Quicksort_1_Partition.py

--- Python_3.4_Hackerrank_Algorithms/Sorting/Quicksort_1_Partition.py
+++ Python_3.4_Hackerrank_Algorithms/Sorting/Quicksort_1_Partition.py
@@ -1,1 +1,18 @@
 
+# I actually wrote this. No lie.
+n = int(input().strip())
+sort = [int(num) for num in input().strip().split(' ')]
+
+p = sort[0]
+equal = [sort[0]]
+left = []
+right = []
+for i in range(1,n):
+    if sort[i] > p:
+        right.append(sort[i])
+    elif sort[i] < p:
+        left.append(sort[i])
+    else:
+        equal.append(sort[i])
+        
+print(' '.join(str(num) for num in left) , ' '.join(str(num) for num in equal) , ' '.join(str(num) for num in right))
