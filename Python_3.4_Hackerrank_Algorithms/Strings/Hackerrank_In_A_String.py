bd55f6c9cd57da52214b11b084e9dfcd1c9addac
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 12:14:05 -0500

    Update Hackerrank_In_A_String.py

--- Python_3.4_Hackerrank_Algorithms/Strings/Hackerrank_In_A_String.py
+++ Python_3.4_Hackerrank_Algorithms/Strings/Hackerrank_In_A_String.py
@@ -1,1 +1,35 @@
 
+#!/bin/python3
+
+import sys
+
+ref = "hackerrank"
+length = len(ref)
+q = int(input().strip())
+
+for a0 in range(q):
+    s = input().strip()
+    # your code goes here
+    index = [-1]
+    new = ""
+    
+    for i in s:
+        if i in ref:
+            new += i
+            
+    for j in range(length):
+        for i in range(j,len(new)):
+            if new[i] == ref[j] and i not in index and i > max(index):
+                index.append(i)
+                break
+                
+    index.remove(index[0])
+    if len(index) == length and index == sorted(index):
+        print("YES")
+    else:
+        print("NO")
+    
+                
+    
+        
+            
