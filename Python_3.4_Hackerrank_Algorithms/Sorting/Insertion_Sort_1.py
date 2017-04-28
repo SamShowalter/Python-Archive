136746a63543a870dc494d462ae0dbc173cc2db4
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 13:22:18 -0500

    Update Insertion_Sort_1.py

--- Python_3.4_Hackerrank_Algorithms/Sorting/Insertion_Sort_1.py
+++ Python_3.4_Hackerrank_Algorithms/Sorting/Insertion_Sort_1.py
@@ -1,1 +1,16 @@
+n = int(input().strip())
+alist = [int(num) for num in input().strip().split(" ")]
 
+def insertionSort(alist):
+   for index in range(1,len(alist)):
+       currentvalue = alist[index]
+        position = index
+        while position>0 and alist[position-1]>currentvalue:
+              alist[position]=alist[position-1]
+              print(*alist, sep = ' ')
+              position = position-1
+
+          alist[position]=currentvalue
+
+insertionSort(alist)
+print(*alist, sep = ' ')
