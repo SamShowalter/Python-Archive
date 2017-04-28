8da68ec3223c0921be9ac93c1b46e20a34209dc4
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 12:15:27 -0500

    Update Pangrams.py

--- Python_3.4_Hackerrank_Algorithms/Strings/Pangrams.py
+++ Python_3.4_Hackerrank_Algorithms/Strings/Pangrams.py
@@ -1,1 +1,16 @@
 
+s = input().strip().replace(" ","")
+ref = "abcdefghijklmnopqrstuvwxyz"
+result = ""
+answer = ""
+
+for i in range(len(s)):
+    if s[i].lower() not in result:
+            result += (s[i].lower())
+        
+result = ''.join(sorted(result))
+
+if result == ref:
+    print("pangram")
+else:
+    print("not pangram")
