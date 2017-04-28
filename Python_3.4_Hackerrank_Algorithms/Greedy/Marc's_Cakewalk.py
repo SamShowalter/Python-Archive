92e16aeea79691b97832a7bd7f67f48be897453d
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 13:37:02 -0500

    Update Marc's_Cakewalk.py

--- Python_3.4_Hackerrank_Algorithms/Greedy/Marc's_Cakewalk.py
+++ Python_3.4_Hackerrank_Algorithms/Greedy/Marc's_Cakewalk.py
@@ -1,1 +1,15 @@
 
+#!/bin/python3
+
+import sys
+
+n = int(input().strip())
+calories = list(map(int, input().strip().split(' ')))
+calbest = sorted(calories, reverse = True)
+milecount = 0
+
+for i in range(n):
+    milecount += (2**i)*calbest[i]
+    
+print(milecount)
+    
