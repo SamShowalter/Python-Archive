4cb8d2178d51907ad4c3954e1547c8cb7556ee2e
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 20:06:37 -0500

    Update Find_Digits.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Find_Digits.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Find_Digits.py
@@ -1,1 +1,20 @@
 
+#!/bin/python3
+
+import sys
+
+t = int(input().strip())
+
+def getDigit(n,k):
+    n = str(n)
+    return int(n[k-1])
+
+for a0 in range(t):
+    n = str(input().strip())
+    count = 0
+    for i in range(len(n)):
+        if (int(n[i]) == 0):
+            count += 0
+        elif int(n) % int(n[i]) == 0:
+            count += 1
+    print(count)
