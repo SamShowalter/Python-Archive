ea4e067d8d7497d4382a38a7eea9e8a82c96ec80
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 20:41:21 -0500

    Update Day_Of_The_Programmer.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Day_Of_The_Programmer.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Day_Of_The_Programmer.py
@@ -1,1 +1,18 @@
 
+import sys
+
+year = int(input().strip())
+
+def solve(year):
+    days_total = 243
+    if (1700 <= year <= 1917) and (year%4 == 0):
+        days_total += 1
+    if (year == 1918):
+        days_total = 230    
+    if 1919 <= year and (year % 400==0 or  (year % 4==0 and year % 100!=0)):
+        days_total += 1
+    dd = 256 - days_total
+    return str(dd)+"."+"09"+"."+str(year)
+
+result = solve(year)
+print(result)
