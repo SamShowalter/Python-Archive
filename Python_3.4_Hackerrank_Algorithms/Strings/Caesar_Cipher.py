1608655cb83857240e43090f07fa7e669103fba5
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 12:06:14 -0500

    Update Caesar_Cipher.py

--- Python_3.4_Hackerrank_Algorithms/Strings/Caesar_Cipher.py
+++ Python_3.4_Hackerrank_Algorithms/Strings/Caesar_Cipher.py
@@ -1,1 +1,22 @@
 
+#!/bin/python3
+
+import sys
+import string
+
+labc = string.ascii_lowercase
+uabc = string.ascii_uppercase
+n = int(input().strip())
+string = str(input().strip())
+rotate  = int(input().strip())
+newstring = ""
+
+for i in string:
+  if (i in labc):
+      newstring += chr((ord(i) - 97 + rotate)%26 + 97)
+  elif (i in uabc):
+      newstring += chr((ord(i) - 65 + rotate)%26 + 65)
+  else:
+    newstring += i
+
+print(newstring)
