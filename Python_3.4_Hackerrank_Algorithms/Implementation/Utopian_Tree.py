cc3677d68b8a9cce2db7a087d04f863b5579248d
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 19:57:09 -0500

    Update Utopian_Tree.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Utopian_Tree.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Utopian_Tree.py
@@ -1,1 +1,30 @@
 
+#!/bin/python3
+
+import sys
+
+t = int(input().strip())
+    
+def getHeight(n):
+    beg = 1
+    temp = 0
+    if (n == 0): 
+        return 1
+    if (n % 2 == 0):
+        while temp < n:
+            beg = beg * 2
+            beg += 1
+            temp += 2
+        return beg
+    else:
+        while temp < (n-1):
+            beg = beg * 2
+            beg += 1
+            temp += 2
+        return beg * 2
+
+for a0 in range(t):
+    n = int(input().strip())
+    print(getHeight(n))
+
+        
