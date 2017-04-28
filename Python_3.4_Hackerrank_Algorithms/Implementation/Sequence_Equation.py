a63842b886ea20ee8e82ea91d05ac67e49781b96
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 20:04:26 -0500

    Update Sequence_Equation.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Sequence_Equation.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Sequence_Equation.py
@@ -1,1 +1,12 @@
 
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+count = int(input().strip())
+nums = [int(num) for num in input().strip().split(' ')]
+
+for i in range(1,len(nums)+1):
+    if i in nums:
+        temp = (nums.index(i) + 1)
+        if temp in nums:
+            print(nums.index(temp) + 1) 
+            
+
