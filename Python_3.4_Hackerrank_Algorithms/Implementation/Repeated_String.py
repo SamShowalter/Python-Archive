1700214d57835813871cba7827d8d32ba2379e6d
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 20:11:12 -0500

    Update Repeated_String.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Repeated_String.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Repeated_String.py
@@ -1,1 +1,22 @@
 
+#!/bin/python3
+
+import sys
+
+s = input().strip()
+n = int(input().strip())
+fullcount = 0
+partcount = 0
+length = len(s)
+
+for i in s:
+    if i == 'a':
+        fullcount += 1
+        
+for i in range(n%length):
+    if s[i] == 'a':
+        partcount += 1
+        
+print((n//length)*fullcount + partcount)
+               
+               
