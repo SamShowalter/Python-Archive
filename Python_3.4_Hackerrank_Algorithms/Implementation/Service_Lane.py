6a833187a12f9f59a07fef3f43a307750dd11de1
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 20:23:49 -0500

    Update Service_Lane.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Service_Lane.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Service_Lane.py
@@ -1,1 +1,19 @@
 
+#!/bin/python3
+
+import sys
+
+n,t = input().strip().split(' ')
+n,t = [int(n),int(t)]
+width = [int(width_temp) for width_temp in input().strip().split(' ')]
+
+for a0 in range(t):
+    i,j = input().strip().split(' ')
+    i,j = [int(i),int(j)]
+    tempwidth = width[i: j+1]
+    answer = min(tempwidth)
+    
+    print(answer)
+
+
+    
