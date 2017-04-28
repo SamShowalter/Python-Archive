2ff2c5bf5afeb835da06eb6494109840f7b4bd69
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 20:09:59 -0500

    Update Cut_The_Sticks.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Cut_The_Sticks.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Cut_The_Sticks.py
@@ -1,1 +1,20 @@
 
+#!/bin/python3
+
+import sys
+
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+
+while len(arr) > 0:
+    tiny = min(arr)
+    for i in range(len(arr)):
+        arr[i] -= tiny
+    newarr = []
+    for i in range(len(arr)):
+        if arr[i] > 0:
+            newarr.append(arr[i])
+    arr = newarr
+    if len(arr) != 0:
+        print(len(arr))
+                 
