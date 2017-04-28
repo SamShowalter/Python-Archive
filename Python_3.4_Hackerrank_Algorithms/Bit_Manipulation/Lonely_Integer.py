e4b98cdb8c9818f34e5a0d4ee1867007df0aa8d8
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 13:53:45 -0500

    Update Lonely_Integer.py

--- Python_3.4_Hackerrank_Algorithms/Bit_Manipulation/Lonely_Integer.py
+++ Python_3.4_Hackerrank_Algorithms/Bit_Manipulation/Lonely_Integer.py
@@ -1,1 +1,16 @@
 
+#!/usr/bin/py
+# Head ends here
+from collections import Counter
+
+n = int(input().strip())
+alist = [int(num) for num in input().strip().split(' ')]
+
+def lonelyinteger(b):
+    x = Counter(b)
+    for letter, count in list(x.most_common(100)):
+        if count == 1:
+            answer = letter
+    return answer
+
+print(lonelyinteger(alist))
