850495f34d778184a6f50b8b193b649bf08104aa
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 20:17:38 -0500

    Update Taum_And_Bday.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Taum_And_Bday.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Taum_And_Bday.py
@@ -1,1 +1,26 @@
 
+#!/bin/python3
+
+import sys
+
+t = int(input().strip())
+
+for a0 in range(t):
+    b,w = input().strip().split(' ')
+    b,w = [int(b),int(w)]
+    x,y,z = input().strip().split(' ')
+    x,y,z = [int(x),int(y),int(z)]
+    
+    #black gifts
+    cost = 0
+    if y + z < x:
+        cost += b*(y+z)
+    else:
+        cost += b*x
+
+    #white gifts
+    if x + z < y:
+        cost += (x + z)*w
+    else:
+        cost += y * w
+    print(cost)
