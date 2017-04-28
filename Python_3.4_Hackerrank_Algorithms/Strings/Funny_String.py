550f261ac64003a23e3df3a924968e6dfc00cdf3
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 12:23:01 -0500

    Update Funny_String.py

--- Python_3.4_Hackerrank_Algorithms/Strings/Funny_String.py
+++ Python_3.4_Hackerrank_Algorithms/Strings/Funny_String.py
@@ -1,1 +1,18 @@
 
+n = int(input().strip())
+
+for a0 in range(n):
+    x = ""
+    s = input().strip()
+    #Reverse the order of string s
+    r = s[::-1]
+    
+    for i in range(1,len(s)):
+        if abs(ord(s[i]) - ord(s[i-1])) != abs(ord(r[i]) - ord(r[i-1])):
+            x = "Not Funny"
+    if x == "Not Funny":
+        print(x)
+    else:
+        print("Funny")
+        
+    
