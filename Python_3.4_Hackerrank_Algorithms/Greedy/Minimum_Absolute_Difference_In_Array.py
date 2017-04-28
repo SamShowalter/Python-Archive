1a5632236a5aab2c3043e6b6a583efc3b34ef9ea
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 13:42:19 -0500

    Update Minimum_Absolute_Difference_In_Array.py

--- Python_3.4_Hackerrank_Algorithms/Greedy/Minimum_Absolute_Difference_In_Array.py
+++ Python_3.4_Hackerrank_Algorithms/Greedy/Minimum_Absolute_Difference_In_Array.py
@@ -1,1 +1,21 @@
 
+#!/bin/python3
+
+import sys
+
+n = int(input().strip())
+a = list(map(int, input().strip().split(' ')))
+b = sorted(a)
+mindif = max(b) + 1
+
+for i in range(n-1):
+    dif = abs(b[i] - b[i + 1])
+    if dif < mindif:
+         mindif = dif
+            
+print(mindif)       
+        
+        
+        
+        
+    
