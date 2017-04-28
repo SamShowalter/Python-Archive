788a75f9ba3f941d958c7fbdc710ccf09feb8f87
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 20:12:04 -0500

    Update Jumping_On_The_Clouds.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Jumping_On_The_Clouds.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Jumping_On_The_Clouds.py
@@ -1,1 +1,19 @@
 
+#!/bin/python3
+
+import sys
+
+n = int(input().strip())
+c = [int(c_temp) for c_temp in input().strip().split(' ')]
+cloud  = 0
+count = 0
+
+while cloud <= (len(c) - 1):
+    if (cloud + 2) <= (n - 1) and (c[cloud+2] != 1):
+        cloud += 2
+    else:
+        cloud += 1
+    count += 1
+    
+print(count-1)
+
