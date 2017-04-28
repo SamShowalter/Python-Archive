0e4d2330125ac168c78ff4a9de31dc897a340a5b
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 19:02:00 -0500

    Update Cat_And_Mouse.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Cat_And_Mouse.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Cat_And_Mouse.py
@@ -1,1 +1,15 @@
 
+import sys
+
+#Fairly straightforward.
+q = int(input().strip())
+
+for a0 in range(q):
+    x,y,z = input().strip().split(' ')
+    x,y,z = [int(x),int(y),int(z)]
+    if (abs(z - x) == abs(y - z)):
+        print("Mouse C")
+    elif (abs(z - x) > abs(y - z)):
+        print("Cat B")
+    else:
+        print("Cat A")
