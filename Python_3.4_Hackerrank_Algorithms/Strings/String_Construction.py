97d0415126e7ae1e3fbbde2588f4a29b98eb1803
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 13:12:58 -0500

    Update String_Construction.py

--- Python_3.4_Hackerrank_Algorithms/Strings/String_Construction.py
+++ Python_3.4_Hackerrank_Algorithms/Strings/String_Construction.py
@@ -1,1 +1,19 @@
 
+#!/bin/python3
+
+import sys
+
+n = int(input().strip())
+
+for a0 in range(n):
+    fcount = 0
+    s = input().strip()
+    final = ""
+    
+    for i in s:
+        if i not in final:
+            fcount += 1
+            final += i
+    print(fcount)
+
+
