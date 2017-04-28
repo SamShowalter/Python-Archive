33bb77418667fa57ac57e22da964536631c55c6d
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 11:59:01 -0500

    Update CamelCase.py

--- Python_3.4_Hackerrank_Algorithms/Strings/CamelCase.py
+++ Python_3.4_Hackerrank_Algorithms/Strings/CamelCase.py
@@ -1,1 +1,11 @@
 
+import sys
+
+#Could also import string to get the alphabet in upper case
+s = input().strip()
+abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+count = 0
+for i in s:
+    if i in abc:
+        count += 1
+print(count + 1)
