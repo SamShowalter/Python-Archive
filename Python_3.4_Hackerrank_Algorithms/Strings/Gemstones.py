5ff5b256f9f2d5b73fe29924a8a36688e8a650d5
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 12:25:13 -0500

    Update Gemstones.py

--- Python_3.4_Hackerrank_Algorithms/Strings/Gemstones.py
+++ Python_3.4_Hackerrank_Algorithms/Strings/Gemstones.py
@@ -1,1 +1,20 @@
 
+import string
+
+n = int(input().strip())
+gemlist = []
+#Full alphabet, but lowercase    
+abc = string.ascii_lowercase
+
+for a0 in range(n):
+    s = str(input().strip())
+    gemlist.append(s)
+
+for i in string.ascii_lowercase:
+    for n in gemlist:
+        if i not in  n:
+            abc = abc.replace(i,'')
+                  
+print(len(abc))          
+
+       
