1c03be569197c6bd3ed6e02a7b1c53af6d18a087
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 20:22:40 -0500

    Update Chocolate_Feast.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Chocolate_Feast.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Chocolate_Feast.py
@@ -1,1 +1,24 @@
 
+#!/bin/python3
+
+import sys
+
+t = int(input().strip())
+
+for a0 in range(t):
+    n,c,m = input().strip().split(' ')
+    money,cost,wraptrade = [int(n),int(c),int(m)]
+    initial = (money // cost)
+    total = initial
+    wrapcount = initial
+    
+    while wrapcount >= wraptrade:
+        temp = (wrapcount // wraptrade)
+        total += temp
+        wrapcount -= (temp * wraptrade)
+        wrapcount += temp
+        
+    print(total)
+    
+    
+    
