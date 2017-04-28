081e4bc7479fa67f155eb7b193326bcdf4172495
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 13:52:37 -0500

    Update Fibonnacci_Modified.py

--- Python_3.4_Hackerrank_Algorithms/Dynamic_Programming/Fibonnacci_Modified.py
+++ Python_3.4_Hackerrank_Algorithms/Dynamic_Programming/Fibonnacci_Modified.py
@@ -1,1 +1,30 @@
 
+t1,t2,n = input().strip().split(' ')
+t1,t2,n = [int(t1),int(t2),int(n)]
+
+#Iteratively defining fibonnaci sequence may be a sin.
+def nthFiboMod(t1,t2, n):
+    count = 2
+    while count < n:
+        t3 = t1 + t2**2
+        t1 = t2
+        t2 = t3
+        count += 1
+    return t3
+
+print(nthFiboMod(t1,t2,n))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
