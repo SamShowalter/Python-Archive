e43c021dec147a7a92a434220d2136a38b7a47a1
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 13:39:24 -0500

    Update Mark_And_Toys.py

--- Python_3.4_Hackerrank_Algorithms/Greedy/Mark_And_Toys.py
+++ Python_3.4_Hackerrank_Algorithms/Greedy/Mark_And_Toys.py
@@ -1,1 +1,21 @@
 
+n,k = input().strip().split(' ')
+n, k = int(n), int(k)
+
+alist = [int(num) for num in input().strip().split(' ') if int(num) < k]
+alist = sorted(alist)
+
+s = 0
+i = 0
+count = 0
+newlist = []
+
+while k - (s + alist[i]) > 0 and (i < len(alist)):
+    s += alist[i]
+    newlist.append(alist[i])
+    count += 1
+    i += 1
+    if i == len(alist):
+        break
+    
+print(count)
