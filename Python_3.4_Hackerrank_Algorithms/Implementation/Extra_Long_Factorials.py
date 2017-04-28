4c04e8a68afae7d2fd29f76ef98827dfadde6088
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 20:07:34 -0500

    Update Extra_Long_Factorials.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Extra_Long_Factorials.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Extra_Long_Factorials.py
@@ -1,1 +1,16 @@
 
+#!/bin/python3
+
+import sys
+
+n = int(input().strip())
+
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        #Recursion is used here
+        return n * fact(n-1)
+    
+print(fact(n))
+
