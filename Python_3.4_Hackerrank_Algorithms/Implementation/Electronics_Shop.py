81edcf1a3aac15eb5bed15cd84789af667b4a501
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 19:00:47 -0500

    Update Electronics_Shop.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Electronics_Shop.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Electronics_Shop.py
@@ -1,1 +1,27 @@
 
+import sys
+
+#Thise careful about where and why you are indenting. Contextualize what is happening.
+money, nk, nusb = [int(num) for num in input().strip().split(' ')]
+kc = [int(num) for num in input().strip().split(' ')]
+kusb = [int(num) for num in input().strip().split(' ')]
+
+def electronics():
+    best = 0
+    costdif = 0
+    mindif = money
+    cost = 0
+    for i in range(nk):
+        for j in range(nusb):
+            cost = kc[i] + kusb[j]
+            if cost <= money:
+                costdif = money - cost
+                if costdif < mindif:
+                    mindif = costdif
+                    best = cost
+    if best == 0:
+        return -1
+    else:
+        return best      
+    
+print(electronics())
