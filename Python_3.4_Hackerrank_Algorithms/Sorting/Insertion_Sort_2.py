cfa90c42a8c6a565b05fdfcc7cb6e19cce91e8be
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 13:23:26 -0500

    Update Insertion_Sort_2.py

--- Python_3.4_Hackerrank_Algorithms/Sorting/Insertion_Sort_2.py
+++ Python_3.4_Hackerrank_Algorithms/Sorting/Insertion_Sort_2.py
@@ -1,1 +1,17 @@
 
+n = int(input().strip())
+sort = [int(num) for num in input().strip().split(' ')]
+
+def insertionSort(sort):
+    for i in range(1, len(sort)):
+        temp = sort[i]
+        j = i
+        while j > 0 and temp < sort[j-1]:
+            sort[j] = sort[j-1]
+            j -= 1
+        sort[j] = temp
+        print(' '.join(str(num) for num in sort))
+
+insertionSort(sort)
+            
+        
