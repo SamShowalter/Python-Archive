8c89d6eafa722a90fc5ed5a6ec6056e13b036e30
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 13:07:49 -0500

    Update Game_Of_Thrones_1.py

--- Python_3.4_Hackerrank_Algorithms/Strings/Game_Of_Thrones_1.py
+++ Python_3.4_Hackerrank_Algorithms/Strings/Game_Of_Thrones_1.py
@@ -1,1 +1,18 @@
 
+
+from collections import Counter
+
+s = input().strip()
+c = Counter(s)
+l = list(c.values())
+odd = 0
+
+for i in l:
+    if i%2 != 0:
+        odd += 1
+
+if odd <= 1:
+    print("YES")
+else:
+    print("NO")
+        
