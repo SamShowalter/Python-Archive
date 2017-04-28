9f15d701171ffcfdf4ea7f10dbd2569820954906
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-27 20:24:49 -0500

    Update Manasa_And_Stones.py

--- Python_3.4_Hackerrank_Algorithms/Implementation/Manasa_And_Stones.py
+++ Python_3.4_Hackerrank_Algorithms/Implementation/Manasa_And_Stones.py
@@ -1,1 +1,17 @@
 
+n = int(input().strip())
+
+for a0 in range(n):
+    stone = int(input().strip())
+    difa = int(input().strip())
+    difb = int(input().strip())
+    flist =[]
+    start = 0
+    count = stone
+    
+    for i in range(count):
+        flist.append(difa*i + difb*(count - 1 - i))
+        
+    print(' '.join(str(num) for num in sorted(set(flist))))
+            
+    
