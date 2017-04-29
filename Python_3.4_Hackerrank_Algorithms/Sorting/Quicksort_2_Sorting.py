dba895c69e4f6825a0f47f292c0dc8b2901ce792
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 19:31:56 -0500

    Update Quicksort_2_Sorting.py

--- Python_3.4_Hackerrank_Algorithms/Sorting/Quicksort_2_Sorting.py
+++ Python_3.4_Hackerrank_Algorithms/Sorting/Quicksort_2_Sorting.py
@@ -1,1 +1,17 @@
 
+
+n = int(input().strip())
+alist = [ int(num) for num in input().strip().split(' ')]
+
+#Understanding the recursion for this quicksort algorithm can be tricky
+def quicksort(alist):
+    if len(alist) <= 1:
+        return alist
+    else:
+        pivot = alist[0]
+        l , pivot , r = [num for num in alist if num < pivot], [num for num in alist if num == pivot], [num for num in alist if num > pivot]
+        subarr = quicksort(l) + pivot + quicksort(r)
+        print(' '.join(map(str,subarr)))
+        return subarr
+    
+quicksort(alist)
