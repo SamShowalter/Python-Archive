49481f1dc7e1678bcdf40ec79b7b50f6487cd6d7
Sam Showalter <samuelrshowalter@gmail.com>
2017-04-28 19:39:57 -0500

    Update Counting_Sort_1.py

--- Python_3.4_Hackerrank_Algorithms/Sorting/Counting_Sort_1.py
+++ Python_3.4_Hackerrank_Algorithms/Sorting/Counting_Sort_1.py
@@ -1,1 +1,14 @@
 
+
+n = int(input().strip())
+
+sort = [int(num) for num in input().strip().split(' ')]
+
+count = []
+for i in range(100):
+    count.append(0)
+    
+for i in range(n):
+    count[sort[i]] += 1
+   
+print(' '.join(str(num) for num in count))
